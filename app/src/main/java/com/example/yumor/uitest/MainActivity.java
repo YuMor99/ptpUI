fac67ae593a84c4f090bec6e3d15c010691286d5
YuMor <yumor926@gmail.com>
2018-07-08 12:29:26 +0800

    include gradle config

--- app/src/main/java/com/example/yumor/uitest/MainActivity.java
+++ app/src/main/java/com/example/yumor/uitest/MainActivity.java
@@ -2,25 +2,18 @@ package com.example.yumor.uitest;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.net.Uri;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import java.util.HashMap;
-
-import io.vov.vitamio.LibsChecker;
-import io.vov.vitamio.MediaPlayer;
-import io.vov.vitamio.provider.MediaStore;
-import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
